c6e5b23476e0c7044ae864d9c51f1f5a5c1dbf45
Haonan Liu <haonliu@microsoft.com>
2019-04-05 15:38:31 -0700

    wip

--- src/bug-filing/bug-filing-service-provider.ts
+++ src/bug-filing/bug-filing-service-provider.ts
@@ -2,7 +2,25 @@
 // Licensed under the MIT License.
 import { BugFilingService } from './types/bug-filing-service';
 export class BugFilingServiceProvider {
-    constructor(private readonly services: BugFilingService[]) {}
+    private static instance: BugFilingServiceProvider;
+    private readonly services: BugFilingService[];
+
+    private constructor() {
+        this.services = [];
+    }
+
+    public getInstance() {
+        if (BugFilingServiceProvider.instance === null) {
+            BugFilingServiceProvider.instance = new BugFilingServiceProvider();
+        }
+
+        return BugFilingServiceProvider.instance;
+    }
+
+    public addService(service: BugFilingService): void {
+        this.services.push(service);
+    }
+
     public all(): BugFilingService[] {
         return this.services.slice();
     }
